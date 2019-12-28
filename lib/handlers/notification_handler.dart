8f6ab829b20cf34c1ae58243c9e8ade14082424a
Daniel Berge <daniel@berge.us>
2019-12-28 12:50:19 +0100

    Refactor dart file names

--- lib/handlers/notification_handler.dart
+++ lib/handlers/notification_handler.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:notification_notes/ItemList.dart';
 import 'package:provider/provider.dart';
