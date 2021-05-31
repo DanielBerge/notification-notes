33e9f20b776a64c589f8ec79525370660f30abae
Daniel Berge <daniel@berge.us>
2021-05-31 21:39:26 +0200

    Rename title

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
       ),
       home: ChangeNotifierProvider<NoteListHandler>(
         create: (context) => NoteListHandler(),
-        child: NotificationList(title: 'MY NOTES'),
+        child: NotificationList(title: 'Notifications'),
       ),
     );
   }
